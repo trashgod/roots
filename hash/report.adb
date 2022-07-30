6ea37b3a4d25e62c28c347d6762c05d019cfcf49
trashgod <trashgod@gmail.com>
2022-07-30 18:38:31 -0400

    Refactor report.

--- hash/report.adb
+++ hash/report.adb
@@ -1,8 +1,8 @@
 pragma Warnings ("I"); -- allow internal unit, Prime_Numbers
 with Ada.Containers.Ordered_Maps;
-with Ada.Containers.Prime_Numbers;
-with Ada.Float_Text_IO; use Ada.Float_Text_IO;
-with Ada.Text_IO;       use Ada.Text_IO;
+with Ada.Containers.Prime_Numbers; use Ada.Containers.Prime_Numbers;
+with Ada.Float_Text_IO;            use Ada.Float_Text_IO;
+with Ada.Text_IO;                  use Ada.Text_IO;
 
 package body Report is
 
@@ -10,42 +10,40 @@ package body Report is
      (Key_Type => Natural, Element_Type => Natural);
    type Table_Array is array (Natural range <>) of Natural;
 
-   Hash_Count : Count_Type;
-
-   procedure Print_Count (Position : Maps.Cursor) is
-      Key     : constant Natural := Maps.Key (Position);
-      Element : constant Natural := Maps.Element (Position);
-      Percent : constant Float   := Float (Key * Element) / Float (Hash_Count);
-   begin
-      Put (Natural'Image (Key) & ":");
-      Put (Natural'Image (Element) & " (");
-      Put (Percent * 100.0, 1, 2, 0);
-      Put_Line ("%)");
-   end Print_Count;
-
    procedure Print (Hash_List : Hash_Vectors.Vector) is
-      Size : constant Hash_Type :=
-        Prime_Numbers.To_Prime (Hash_List.Length) - 1;
+      Hash_Count  : constant Count_Type := Hash_List.Length;
+      Size        : constant Hash_Type := To_Prime (Hash_Count) - 1;
+      Load_Factor : constant Float := Float (Hash_Count) / Float (Size);
       Table       : Table_Array (0 .. Natural (Size)) := (others => 0);
       Index       : Natural;
-      Load_Factor : Float;
       Counts      : Maps.Map;
       Position    : Maps.Cursor;
       Inserted    : Boolean;
       Value       : Natural;
+
+      procedure Print_Count (Position : Maps.Cursor) is
+         Key     : constant Natural := Maps.Key (Position);
+         Element : constant Natural := Maps.Element (Position);
+         Percent : constant Float   :=
+           Float (Key * Element) / Float (Hash_Count);
+      begin
+         Put (Natural'Image (Key) & ":");
+         Put (Natural'Image (Element) & " (");
+         Put (Percent * 100.0, 1, 2, 0);
+         Put_Line ("%)");
+      end Print_Count;
+
    begin
+      Put_Line ("Hash count:" & Count_Type'Image (Hash_Count));
+      Put_Line ("Table size:" & Natural'Image (Table'Length));
+      Put ("Load factor: ");
+      Put (Load_Factor * 100.0, 1, 2, 0);
+      Put_Line ("%");
       --  Count collisions
-      Hash_Count := Hash_List.Length;
       for Element of Hash_List loop
          Index         := Natural (Element mod Table'Length);
          Table (Index) := Table (Index) + 1;
       end loop;
-      Put_Line ("Hash count:" & Natural'Image (Natural (Hash_Count)));
-      Put_Line ("Table size:" & Natural'Image (Table'Length));
-      Load_Factor := Float (Hash_Count) / Float (Table'Length);
-      Put ("Load factor: ");
-      Put (Load_Factor * 100.0, 1, 2, 0);
-      Put_Line ("%");
       --  Tally results
       for Element of Table loop
          Counts.Insert (Element, 1, Position, Inserted);
